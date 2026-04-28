4dbe509fc061aacb894e45d769f82c4e77df21bf
Vahid Nasiri <vahid.nasiri@gmail.com>
2026-04-28 23:43:22 +0330

    Update Program.cs

--- src/DntSdkMirror/Program.cs
+++ src/DntSdkMirror/Program.cs
@@ -3,6 +3,9 @@ using DntSdkMirror.Services.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ??
+                              throw new InvalidOperationException(message: "dirName is null"));
+
 await Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) => services.AddServices(context.Configuration))
     .Build()
