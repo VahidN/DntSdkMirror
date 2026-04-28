abc3e089b39b6c590c896fb97ce71989a6c21e44
Vahid Nasiri <vahid.nasiri@gmail.com>
2026-04-28 22:16:58 +0330

    Create TestsAppFactory.cs

---
+++ src/DntSdkMirror.Tests/Factory/TestsAppFactory.cs
@@ -0,0 +1,29 @@
+using DntSdkMirror.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DntSdkMirror.Tests.Factory;
+
+internal static class TestsAppFactory
+{
+    private static readonly Lazy<IHost> HostProvider = new(GetHost, LazyThreadSafetyMode.ExecutionAndPublication);
+
+    /// <summary>
+    ///     A lazy loaded thread-safe singleton
+    /// </summary>
+    public static IHost Host { get; } = HostProvider.Value;
+
+    public static T GetRequiredService<T>()
+        where T : notnull
+        => Host.Services.GetRequiredService<T>();
+
+    private static IHost GetHost()
+        => Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                var httpClientBuilder = services.AddServices(context.Configuration);
+                services.AddSingleton<FakeHttpMessageHandler>();
+                httpClientBuilder.AddHttpMessageHandler<FakeHttpMessageHandler>();
+            })
+            .Build();
+}
