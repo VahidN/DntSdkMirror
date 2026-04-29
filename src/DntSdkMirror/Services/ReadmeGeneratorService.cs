72b90d4651b458c33800ce9a4c7c8890e415de8b
Vahid Nasiri <vahid.nasiri@gmail.com>
2026-04-29 12:20:00 +0330

    Update ReadmeGeneratorService.cs

--- src/DntSdkMirror/Services/ReadmeGeneratorService.cs
+++ src/DntSdkMirror/Services/ReadmeGeneratorService.cs
@@ -1,29 +1,39 @@
 using DntSdkMirror.Models;
 using DntSdkMirror.Services.Contracts;
 using DntSdkMirror.Utils;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace DntSdkMirror.Services;
 
-public class ReadmeGeneratorService(IAppPathService appPathService, IOptions<AppConfig> appConfig)
-    : IReadmeGeneratorService
+public class ReadmeGeneratorService(
+    IAppPathService appPathService,
+    IOptions<AppConfig> appConfig,
+    ILogger<ReadmeGeneratorService> logger) : IReadmeGeneratorService
 {
     private const string TableSeparator = "<!---->";
 
     public void UpdateReadmeFile()
     {
-        if (!File.Exists(appPathService.ReadmeFilePath))
+        var readmeFilePath = appPathService.ReadmeFilePath;
+
+        if (!File.Exists(readmeFilePath))
         {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(message: "`{File}` doesn't exists.", readmeFilePath);
+            }
+
             return;
         }
 
-        var readmeFileContent = File.ReadAllText(appPathService.ReadmeFilePath);
+        var readmeFileContent = File.ReadAllText(readmeFilePath);
         var mainContent = readmeFileContent.Split(TableSeparator, StringSplitOptions.RemoveEmptyEntries)[0];
 
         ICollection<ICollection<string>> rows = [];
         var sdksDirInfo = new DirectoryInfo(appPathService.OutputFolderPath);
 
-        foreach (var fileInfo in sdksDirInfo.GetFiles(searchPattern: "*.*", SearchOption.AllDirectories))
+        foreach (var fileInfo in sdksDirInfo.GetFiles(searchPattern: "*.exe", SearchOption.AllDirectories))
         {
             rows.Add([
                 fileInfo.Directory?.Name ?? "", GetDownloadLink(fileInfo), fileInfo.Length.ToFormattedFileSize()
@@ -32,7 +42,22 @@ public class ReadmeGeneratorService(IAppPathService appPathService, IOptions<App
 
         var sdksTableContent = MarkdownTableGenerator.GenerateMarkdownTable(["کانال", "فایل", "حجم"], rows);
 
-        File.WriteAllText(appPathService.ReadmeFilePath, $"{mainContent}{TableSeparator}\n{sdksTableContent}");
+        if (string.IsNullOrWhiteSpace(sdksTableContent))
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(message: "Skipped updating `{File}`. There's nothing new to add.", readmeFilePath);
+            }
+
+            return;
+        }
+
+        File.WriteAllText(readmeFilePath, $"{mainContent}{TableSeparator}\n{sdksTableContent}");
+
+        if (logger.IsEnabled(LogLevel.Debug))
+        {
+            logger.LogDebug(message: "Finished updating `{File}`.", readmeFilePath);
+        }
     }
 
     private string GetDownloadLink(FileInfo fileInfo)
