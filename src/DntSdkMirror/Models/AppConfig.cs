4948de6f303098589a03cb787c50749d41a1caac
Vahid Nasiri <vahid.nasiri@gmail.com>
2026-04-29 12:18:55 +0330

    Update AppConfig.cs

--- src/DntSdkMirror/Models/AppConfig.cs
+++ src/DntSdkMirror/Models/AppConfig.cs
@@ -2,15 +2,17 @@ namespace DntSdkMirror.Models;
 
 public class AppConfig
 {
-    public required string OutputFolderName { set; get; }
+    [Required] public string? OutputFolderName { set; get; }
 
-    public required string ReleasesIndexUrl { set; get; }
+    [Required] public string? ReleasesIndexUrl { set; get; }
 
-    public required string ReadmeFileName { set; get; }
+    [Required] public string? ReadmeFileName { set; get; }
 
-    public required string Owner { set; get; }
+    [Required] public string? Owner { set; get; }
 
-    public required string Repository { set; get; }
+    [Required] public string? Repository { set; get; }
 
-    public required string Branch { set; get; }
+    [Required] public string? Branch { set; get; }
+
+    public bool DownloadOneFileEachTime { set; get; }
 }
