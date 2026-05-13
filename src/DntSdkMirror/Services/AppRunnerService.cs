1af2e055c01b8c602d42c36f52906179ff386d95
Vahid Nasiri <vahid.nasiri@gmail.com>
2026-05-13 12:57:30 +0330

    Update AppRunnerService.cs

--- src/DntSdkMirror/Services/AppRunnerService.cs
+++ src/DntSdkMirror/Services/AppRunnerService.cs
@@ -5,12 +5,15 @@ namespace DntSdkMirror.Services;
 /// <summary>
 ///     Defines the entry point of the application
 /// </summary>
-public class AppRunnerService(IFetchSDKsService fetchSdKsService, IReadmeGeneratorService readmeGeneratorService)
-    : IAppRunnerService
+public class AppRunnerService(
+    IFetchSDKsService fetchSdKsService,
+    IReadmeGeneratorService readmeGeneratorService,
+    IChannelReleasesStoreService channelReleasesStoreService) : IAppRunnerService
 {
     public async Task<bool> StartAsync(string[] args)
     {
         var success = await fetchSdKsService.StartAsync(args);
+        channelReleasesStoreService.DeleteAllOldReleases();
         readmeGeneratorService.UpdateReadmeFile();
 
         return success;
